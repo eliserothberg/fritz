e635edbd5941b671efc1950189afda5b38388be4
eliserothberg <rothberg.elise@gmail.com>
2017-08-07 12:20:41 -0700

    adding react scroll to pacage.json, adding .gitignore that should have been there

--- src/components/RouteNavItem.js
+++ src/components/RouteNavItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { NavItem } from 'react-bootstrap';
 
+//enable router
 export default (props) => (
   <Route path={props.href} exact children={({ match }) => (
     <NavItem {...props} active={ match ? true : false }>{ props.children }</NavItem>
